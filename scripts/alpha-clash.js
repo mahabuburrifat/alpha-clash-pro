bd44254a035afbc6dfaf9ad4dfbe15a85d4e49eb
mahabuburrifat <mahabubur06528@gmail.com>
2024-02-17 03:05:17 +0600

    added script

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,12 @@
+// function play(){
+//     const homeScreenSection = document.getElementById('home-screen');
+//     homeScreenSection.classList.add('hidden');
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+    
+// }
+
+function play(){
+    homeScreenSection('home-screen');
+    playGroundSection('play-ground');
+}
---
+++ scripts/utility.js
@@ -0,0 +1,8 @@
+function homeScreenSection(elementId){
+    const homeScreen = document.getElementById(elementId);
+    homeScreen.classList.add('hidden');
+}
+function playGroundSection(elementId){
+    const playGround = document.getElementById(elementId);
+    playGround.classList.remove('hidden');
+}
